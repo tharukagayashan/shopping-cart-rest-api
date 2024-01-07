3b94b172723385b9c2ebb3978654783ed528afbd
Tharuka Gayashan <tharuka.g@informaticsint.com>
2024-01-07 13:25:58 +0530

    Checkout API

--- src/main/java/com/projects/shoppingcart/ShoppingCartApplication.java
+++ src/main/java/com/projects/shoppingcart/ShoppingCartApplication.java
@@ -5,9 +5,11 @@ import com.projects.shoppingcart.model.reference.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+@Transactional
 @Slf4j
 @SpringBootApplication
 public class ShoppingCartApplication {
